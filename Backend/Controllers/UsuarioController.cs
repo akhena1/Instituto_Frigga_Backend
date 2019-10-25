2e2400b6ff479efdccaf7fdc3521072f910bc0de
Gustavo Marcondes <gu.marcondes2012@gmail.com>
2019-10-25 15:10:43 -0300

    Validação de cpf e cnpj

--- Backend/Controllers/UsuarioController.cs
+++ Backend/Controllers/UsuarioController.cs
@@ -58,6 +58,21 @@ namespace Backend.Controllers
         {
             try
             {
+                bool ValidaDoc = false;
+                if( usuario.CnpjCpf.Length == 11)
+                {
+                    ValidaDoc = ValidaCpf(usuario.CnpjCpf);
+                }
+                if( usuario.CnpjCpf.Length == 14 )
+                {
+                    ValidaDoc = ValidaCnpj(usuario.CnpjCpf);
+                }     
+                
+
+                if(ValidaDoc == false)
+                {
+                    return BadRequest();
+                }
                 await _context.AddAsync(usuario);
                 await _context.SaveChangesAsync();
             }
@@ -66,6 +81,145 @@ namespace Backend.Controllers
                 throw;
             }
             return usuario;
+        }
+
+        public bool ValidaCpf(string cpfUsuario)
+        {
+            cpfUsuario = cpfUsuario.Replace(" ", "").Replace(".", "").Replace("-", "").Replace("/", "");
+            bool resultado    = false;
+            int[] v1 = {10, 9, 8, 7, 6, 5, 4, 3, 2};
+
+            string cpfCalculo = "";
+            int resto         = 0;
+            int calculo       = 0;
+
+            string digito_v1 = "";
+            string digito_v2 = "";
+
+            cpfCalculo = cpfUsuario.Substring(0 , 9);
+
+            for(int i = 0; i <= 8; i++){
+                
+                calculo = calculo + int.Parse(cpfUsuario[i].ToString() )* v1[i];
+            }
+           
+            resto = calculo % 11;
+            calculo = 11 - resto;
+
+            if(calculo > 9 ){
+                 digito_v1 = "0";
+                
+            }else{
+                digito_v1 = calculo.ToString();
+            }
+            if( digito_v1 == cpfUsuario[9].ToString() ){
+                resultado = true;
+
+            }
+
+            int[] v2 = {11, 10, 9, 8, 7 , 6, 5 , 4, 3 , 2};
+            resto = 0;
+            
+            cpfCalculo = cpfCalculo + calculo;
+
+            calculo = 0;
+
+            for(int i = 0; i <= 9; i++){
+                
+                calculo = calculo + int.Parse(cpfUsuario[i].ToString() )* v2[i];
+            }
+
+            resto = calculo % 11;
+            calculo = 11 - resto;
+
+            if(calculo > 9 ){
+                 digito_v2 = "0";
+                
+            }else{
+                digito_v2 = calculo.ToString();
+            }
+            if( digito_v2 == cpfUsuario[9].ToString() ){
+                resultado = true;
+
+            }
+
+            return resultado;
+        }
+
+        public bool ValidaCnpj(string cnpjUsuario){
+
+            bool resultado = false;
+
+            int[] v1 = {5 , 4 , 3 , 2 , 9 , 8 , 7 , 6 , 5 , 4 , 3 , 2};
+            
+            string cnpjCalculo = "";
+            int resto = 0;
+            int calculo = 0;
+
+            string digito_v1 = "";
+            string digito_v2 = "";
+
+            cnpjUsuario = cnpjUsuario.Replace(" ", "");
+            cnpjUsuario = cnpjUsuario.Replace(".", "");
+            cnpjUsuario = cnpjUsuario.Replace("-", "");
+            cnpjUsuario = cnpjUsuario.Replace("/", "");
+
+            cnpjCalculo = cnpjUsuario.Substring(0 , 13);
+
+            
+
+            for(int i = 0; i <= 11; i++){
+
+                calculo = calculo + int.Parse(cnpjUsuario[i].ToString() )* v1[i];
+
+            }
+
+            resto = calculo % 11;
+            calculo = 11 - resto;
+           
+            if(resto < 2){
+
+                digito_v1 = "0";
+
+            }else{
+                digito_v1 = calculo.ToString();
+            }
+            if(digito_v1 == cnpjUsuario[12].ToString() ){
+
+                resultado = true;
+            }
+
+            int[] v2 = {6 , 5 , 4 , 3 , 2 , 9 , 8 , 7 , 6 , 5 , 4 , 3 , 2};
+
+            cnpjCalculo = cnpjCalculo + calculo;
+
+            calculo = 0;
+
+
+
+            for(int i = 0; i <= 12; i++){
+
+                calculo = calculo + int.Parse(cnpjUsuario[i].ToString() )* v2[i];
+            }
+
+            resto = calculo % 11;
+            calculo = 11 - resto;
+           
+            if(resto < 2){
+
+                digito_v2 = "0";
+
+            }else{
+                digito_v2 = calculo.ToString();
+            }
+            if(digito_v2 == cnpjUsuario[13].ToString() ){
+
+                resultado = true;
+            }
+
+
+
+            return  resultado;
         }
         /// <summary>
         /// Atualiza dados em Tipo Usuario
